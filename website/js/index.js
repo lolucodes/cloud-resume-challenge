9b828b57ef6cba9be60b42fa8d226eb0f6610a66
Adebiyi Adegboye <62277795+lolucodes@users.noreply.github.com>
2024-08-04 19:17:17 +0000

    lambda url

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://ebt2idsxwlt4lwy3y34g7far440wyhwc.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://ydh6mc4idmigkzqmev4vrnandu0sbawv.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
